e955b3dcbd89698ca36c5c5dc2c15bd77e6b14e0
Bosang6 <huangzhikang66@gmail.com>
2026-05-12 14:04:23 +0200

    Add GameObject

---
+++ include/GameObject.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "Transform2D.h"
+#include "Sprite.h"
+
+class GameObject
+{
+public:
+    GameObject() = default;
+    explicit GameObject(const std::string& name);
+    ~GameObject() = default;
+
+    GameObject(const GameObject&) = delete;
+    GameObject& operator=(const GameObject&) = delete;
+
+    GameObject(GameObject&&) noexcept = default;
+    GameObject& operator=(GameObject&&) noexcept = default;
+
+public:
+    void SetName(const std::string& name);
+    const std::string& GetName() const;
+
+    void SetActive(bool active);
+    bool IsActive() const;
+
+    Transform2D& GetTransform();
+    const Transform2D& GetTransform() const;
+
+    Sprite* AddSprite(ID3D11ShaderResourceView* texture);
+    void RemoveSprite();
+
+    Sprite* GetSprite();
+    const Sprite* GetSprite() const;
+
+    bool HasSprite() const;
+
+private:
+    std::string m_name;
+    bool m_active = true;
+
+    Transform2D m_transform;
+
+    std::unique_ptr<Sprite> m_sprite;
+};
---
+++ src/GameObject.cpp
@@ -0,0 +1,64 @@
+#include "GameObject.h"
+
+GameObject::GameObject(const std::string& name)
+    : m_name(name)
+{
+}
+
+void GameObject::SetName(const std::string& name)
+{
+    m_name = name;
+}
+
+const std::string& GameObject::GetName() const
+{
+    return m_name;
+}
+
+void GameObject::SetActive(bool active)
+{
+    m_active = active;
+}
+
+bool GameObject::IsActive() const
+{
+    return m_active;
+}
+
+Transform2D& GameObject::GetTransform()
+{
+    return m_transform;
+}
+
+const Transform2D& GameObject::GetTransform() const
+{
+    return m_transform;
+}
+
+Sprite* GameObject::AddSprite(ID3D11ShaderResourceView* texture)
+{
+    m_sprite = std::make_unique<Sprite>();
+    m_sprite->SetTexture(texture);
+
+    return m_sprite.get();
+}
+
+void GameObject::RemoveSprite()
+{
+    m_sprite.reset();
+}
+
+Sprite* GameObject::GetSprite()
+{
+    return m_sprite.get();
+}
+
+const Sprite* GameObject::GetSprite() const
+{
+    return m_sprite.get();
+}
+
+bool GameObject::HasSprite() const
+{
+    return m_sprite != nullptr;
+}
